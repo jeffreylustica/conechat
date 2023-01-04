b0395abed72772123563f6ae2cb4c62da49d82dd
jeffnew1 <jefflustica2@gmail.com>
2023-01-04 16:21:13 +0800

    Updates

---
+++ precache-manifest.ccb7ed42f9fac1c92eedbc6159499e0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975de0fce9b475e140c9d7807c3c143c",
+    "url": "/conechat/index.html"
+  },
+  {
+    "revision": "d0dbf5394b70c1b0d75d",
+    "url": "/conechat/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "9c3344ebc99475b5b848",
+    "url": "/conechat/static/js/2.dd936782.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/conechat/static/js/2.dd936782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0dbf5394b70c1b0d75d",
+    "url": "/conechat/static/js/main.c7c13a02.chunk.js"
+  },
+  {
+    "revision": "72c31184cae729a334fd",
+    "url": "/conechat/static/js/runtime-main.6c1fead1.js"
+  }
+]);
